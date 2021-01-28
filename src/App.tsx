cd6ccb87ce5c1c89dac0f23b63f1648dc7a9ced6
Choharim <jhl8586@naver.com>
2021-01-28 11:18:24 +0900

    Add fakeAPI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import {useState} from 'react';
+import {useQuery} from 'react-query';
+import {Wrapper} from './App.styles';
+
+export type CartItemType  = {
+  id: number;
+  category:string;
+  description:string;
+  image:string;
+  price:number;
+  title:string;
+  amount:number;
+}
+
+const getProducts = async (): Promise<CartItemType[]> => await (await fetch("https://fakestoreapi.com/products")).json();
 
 const App = () => {
+  const {data,isLoading,error} = useQuery<CartItemType[]>('products',getProducts);
+  console.log(data);
   return (
     <div className="App">
      start
